ff6e74b5995b2b3e1fdcb02dc68e061c73e11a84
everdimension <everdimension@gmail.com>
2026-04-30 20:48:14 +0300

    lag radar style improvements

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import { LagRadar } from "@/components/LagRadar";
 
 export default function Page() {
   return (
-    <main className="min-h-screen w-full flex items-start justify-center p-6 sm:p-10">
-      <OrderbookWidget />
-      <LagRadar />
+    <main className="min-h-screen w-full p-6 sm:p-10">
+      <div className="flex flex-col items-start gap-4 w-[420px] max-w-full mx-auto">
+        <OrderbookWidget />
+        <LagRadar />
+      </div>
     </main>
   );
 }
